6d21a7c1f13318e39e104a83eb6d9b47d418c400
ayeshatariq102 <f24605102@nutech.edu.pk>
2025-02-20 11:39:18 +0500

    console in javascript

---
+++ javascript/test.js
@@ -0,0 +1,19 @@
+console.time('this entire code took time..')
+console.log('first file of javascript');
+console.log('hello world');
+console.log(`freeak's world`);
+console.log(100);
+console.log(100+2);
+console.log(100-2);
+console.log(1008*2);
+console.log(100/5);
+console.log(100%19);
+console.log([2,10,20,40,50]);
+console.log([2,'hello worlds',20,40,50]);
+console.log({course:'fullstack',rating:'5'})
+console.table({course:'fullstack',rating:5})
+// console.warn('this is warn');
+// console.error("this is big error")
+console.timeEnd('this entire code took time..')
+// console.clear()
+// this is single line comment
