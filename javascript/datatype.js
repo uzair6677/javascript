c92caf4c8361dd479c8cbf01208f105ba0b62189
ayeshatariq102 <f24605102@nutech.edu.pk>
2025-02-20 13:32:31 +0500

    datatype in javascript

---
+++ javascript/datatype.js
@@ -0,0 +1,56 @@
+// console.log('this is datatype')
+
+
+//primitive(stack)
+/* 
+1. String
+2. Number
+3. Boolean
+4. Null
+5. Undefined
+*/
+let name='fullstack';
+console.log("this course covers " + name + " web development");
+console.log(typeof name);
+
+
+let marks=98;
+console.log("tom scored" + marks)
+console.log("the data type is "+(typeof marks));
+
+let state=true;
+console.log("is this the best web development course " + state)
+console.log("the data type is " + (typeof state));
+
+
+let test1=null;
+console.log(test1);
+console.log("the data type is " + (typeof test1));
+
+
+let test2=undefined;
+console.log("the data type is " + (typeof test2));
+
+
+
+//refrence(heap/derived)
+/* 
+1. array
+2. object literals
+3. function
+4. dates
+*/
+
+let arr=[10,20,30,40,50];
+console.log(arr);
+console.log("the data type is " + (typeof arr));
+
+let students={
+    jack:90,
+    john:95,
+    tom:98,
+}
+console.log(students)
+console.log("the data type is " + (typeof students));
+
+
