a7893a17a48c59b53ed4fed9f8f3735684b70bd1
ayeshatariq102 <f24605102@nutech.edu.pk>
2025-02-22 14:25:51 +0500

    dom

---
+++ javascript/dom.js
@@ -0,0 +1,15 @@
+let a=document; //show all html page
+
+a=document.all; // show all html tags
+a=document.body; //show full body
+a=document.documentElement; //
+a=document.head; //show all inside body
+a=document.anchors; // show a href tag
+a=document.forms; // show form in array and in one formonly 1 element 
+b=document.links; // show links
+b=document.URL; // show ip address of webpage
+b=document.title; // show title
+a=document.scripts; //show script
+console.log(a);
+
+
