bc260f590a832d167512041820327d521a41d763
ayeshatariq102 <f24605102@nutech.edu.pk>
2025-02-22 11:03:47 +0500

    window manipulation and properties

---
+++ javascript/manipulatingwebpageusingjs.js
@@ -0,0 +1,17 @@
+// window object properties
+
+let a=document;
+a=window.innerWidth;
+a=window.innerHeight;
+a=location.toString()
+//scrollx
+//scrolly
+//history.go(-1)
+// console.log(a);
+
+// window object methold
+let b=document;
+// window.alert('this is alert');
+// window.prompt('enter your login id');
+// window.closed;
+window.confirm("are you sure you want to continue");
