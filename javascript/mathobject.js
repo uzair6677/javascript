3bfe145a1d91fcd45fc3afb98928c48bc9f04e7a
ayeshatariq102 <f24605102@nutech.edu.pk>
2025-02-25 10:25:46 +0500

    math objects

---
+++ javascript/mathobject.js
@@ -0,0 +1,34 @@
+let x=3;
+let y=6;
+let z;
+
+// z=x+y;
+// z=x-y;
+// z=x*y;
+// z=x/y;
+// console.log(z)
+ 
+// Math object in javascript
+z=Math;
+z=Math.PI;
+z=Math.E;
+z=Math.round(1.8);
+z=Math.ceil(1.3); //round up
+z=Math.floor(1.8); // round down
+z=Math.abs(-5);
+z=Math.sqrt(81);
+z=Math.pow(2,3);
+z=Math.min(2,3,-51,0,90);
+z=Math.max(2,3,-51,0,90);
+z=Math.random();
+z=100*Math.random();
+z = 50 + (100 - 50) * Math.random();
+z = Math.ceil(50 + (100 - 50) * Math.random());
+z = Math.floor(50 + (100 - 50) * Math.random());
+Math.random(50 + (100 - 50) * Math.random());
+
+
+console.log(z)
+
+
+
