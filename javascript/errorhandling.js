18de2034c69403628a7f74b8aff40f3b54986d1d
ayeshatariq102 <f24605102@nutech.edu.pk>
2025-02-26 12:07:39 +0500

    try and catch and final

---
+++ javascript/errorhandling.js
@@ -0,0 +1,49 @@
+//different kind of error
+
+//1 . syntax error ->occurs where is error in syntax
+
+
+
+//runtime error ->occurs during execution of program
+
+
+
+//type of error >occurs when variable or parameter i not valid type
+
+
+// refrence error =>occur when there is invalid refrence
+
+
+//eval error =>occur when there is error in global function
+
+
+// is your range errornumeric variable or parameter is outside of valid range
+
+// basic syntx of try catch
+
+try{
+    //some code
+}catch(error){
+//this will run if your code in try block faces any error
+}
+//throw and finally
+
+let x='error handling in js is very essy';
+x=undefined;
+if(x!=undefined){
+throw new Error('Random text');
+
+}
+else{
+    console.log('this is undefined');
+    
+}
+fun()
+try{
+    // ndglllfllfl
+    
+}catch(error){
+    console.log('are you sure ? that your code is correct');
+    console.log(error);
+    
+}
